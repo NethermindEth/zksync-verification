df1535d19721adb365309f059df55c920173178f
Fabrício S. Paranhos <fabricio@nethermind.io>
2024-06-04 04:54:57 -0300

    Silence Warnings (#5)

--- script/entrypoint.sh
+++ script/entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-cp $1 ~/.emacs
+echo -e "(setq warning-minimum-level :emergency)\n" > ~/.emacs
+cat $1 >> ~/.emacs
 cd ~/project
 echo "lamport" | sudo -k -S chmod -R a+rw .
 bash --login -c $2
